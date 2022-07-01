46bc84ada1b5725d6ed9a5a47e08342f94a37395
selva kumar <selvakumar1995sri@gmail.com>
2022-07-01 21:00:36 +0530

    commit

--- demo/studentflaskrestful.py
+++ demo/studentflaskrestful.py
@@ -7,24 +7,41 @@ from flask_restful import Resource, Api
 client=pymongo.MongoClient("mongodb+srv://mongouser:mongopwd@cluster1.davwpcs.mongodb.net/?retryWrites=true&w=majority")
 datalist=client["mydatabase"]
 col=datalist["resigtry"]
+user_db=datalist["db"]
 
 app=Flask(__name__)
 
 api = Api(app)
 
-class Student(Resource):
+def auth_api(func):
+    def wrapper(*args,**kwargs):
 
+        print("started")
+        login=request.headers.get("x-api-key")
+        for i in user_db.find({},{"_id":0,"user":1}):
+            if i["user"]==login:
+                resp=func(*args,**kwargs)
+            else:
+                return "Unauthorised user"
+        print("stoped")
+        return resp
+        
+    return wrapper
+
+class Students(Resource):
+
+    @auth_api
     def get(self):
         try:
           
-            x= col.find_one()
-            print(type(x))
-                
-            return x
+            x= col.find({},{"_id":0})
+            
+            return [i for i in x]
+            
         except Exception as e:
             print("error on get_stu_details :" +str(e))
 
-    
+    @auth_api
     def post(self):
         try:
             stu=request.get_json()
@@ -35,7 +52,7 @@ class Student(Resource):
             print("error on get_stu_details :" +str(e))
 
 
-    
+    @auth_api
     def delete():
         try:
             Rno = request.get_json()
@@ -46,7 +63,7 @@ class Student(Resource):
             print("error"+str(e))
 
 
-    
+    @auth_api
     def put():
         try:
             Rno = request.args.get("Rno")
@@ -59,7 +76,7 @@ class Student(Resource):
         except Exception as e:
             print("error on updation :" +str(e))
 
-api.add_resource(Student, '/')
+api.add_resource(Students, '/students')
 
 
 if __name__=='__main__':
